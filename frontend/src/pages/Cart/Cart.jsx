ac0b48bbac428bacf7e28c69ce7a3451ee8c2c83
Sagar <saagarkozu@gmail.com>
2025-09-30 11:04:04 +0545

    Add initial files

---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,77 @@
+import { useContext, React } from 'react'
+import './Cart.css'
+import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from 'react-router-dom';
+
+const Cart = () => {
+
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+
+  const navigate = useNavigate();
+
+  return (
+    <div className='cart'>
+      <div className="items-in-cart"><b>Items added into the cart:</b></div>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className='cart-items-title cart-items-item'>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>Rs. {item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>Rs. {item.price * cartItems[item._id]}</p>
+                  <p className='cross' onClick={() => removeFromCart(item._id)}>x</p>
+                </div>
+                <hr />
+              </div>
+            )
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>Rs. {getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>Rs. {getTotalCartAmount()===0?0:200}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>Rs. {getTotalCartAmount()===0?0:getTotalCartAmount()+200}</b>
+            </div>
+          </div>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+        </div>
+          <div className="cart-promocode">
+            <p>If you have a promo code, enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder='promo code' />
+              <button>Submit</button>
+            </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cart
---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,56 @@
+import React, {useContext} from 'react'
+import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
+
+const PlaceOrder = () => {
+
+  const {getTotalCartAmount} = useContext(StoreContext);
+
+  return (
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First name" required />
+          <input type="text" placeholder="Last name" required />
+        </div>
+        <input type="email" placeholder="Email" required />
+        <input type="text" placeholder='Street' />
+        <div className="multi-field">
+          <input type="text" placeholder="City" required />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-field">
+          <input type="text" placeholder="Zip code" required />
+          <input type="text" placeholder="Country" required />
+        </div>
+        <input type="text" placeholder='Phone' required />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>Rs. {getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>Rs. {getTotalCartAmount()===0?0:200}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>Rs. {getTotalCartAmount()===0?0:getTotalCartAmount()+200}</b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+      
+    </form>
+  )
+}
+
+export default PlaceOrder
