0ebc77153f026946844930a9aadd160fa4240de2
Sagar <saagarkozu@gmail.com>
2025-10-28 12:29:02 +0545

    added backend

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,9 +1,9 @@
-import foodModel form '../models/foodModel.js';
+import foodModel from '../models/foodModel.js';
 import fs from 'fs';
 
 // add food item
 const addFood = async () => {
 
-}
+};
 
 export { addFood };
