c1a0135e30d2580b70d029687f86c1843987e94c
Sagar <saagarkozu@gmail.com>
2025-11-26 12:18:02 +0545

    Improved Payment Functionality

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,77 @@
+import orderModel from '../models/orderModel.js';
+import userModel from '../models/userModel.js';
+import Stripe from 'stripe';
+import 'dotenv/config.js';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET);
+
+// Placing user order for frontend
+const placeOrder = async (req, res) => {
+
+    const frontend_url = "http://localhost:5173";
+    try{
+        const newOrder = new orderModel({
+            userId: req.body.userId,
+            items: req.body.items,
+            amount: req.body.amount,
+            address: req.body.address
+        });
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData:{}});
+
+        const line_items = req.body.items.map((item) => ({
+            price_data:{
+                currency:"npr",
+                product_data:{
+                    name: item.name
+                },
+                unit_amount: item.price*100*135
+            },
+            quantity: item.quantity 
+        }));
+
+        line_items.push({
+            price_data:{
+                currency:"npr",
+                product_data:{
+                    name: "Delivery Charges"
+                },
+                //Time 8:00:00
+                unit_amount: 2*100*135
+            },
+            quantity: 1 
+        });
+
+        const session = await stripe.checkout.sessions.create({
+            line_items: line_items,
+            mode: 'payment',
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+        });
+
+        res.json({success:true, session_url: session.url});
+    }
+    catch (error) {
+        console.log("Error in placing order:", error);
+        res.json({success:false, message:"Error"});
+    }
+};
+
+const verifyOrder = async (req, res) => {
+    const {orderId, success} = req.body;
+    try{
+        if(success === "true"){
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true, message:"Payment Successful"});
+        }
+        else{
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({success:false, message:"Payment Failed"});
+        }
+    }catch(error){
+        console.log(error);
+        res.json({success:false, message:"Failed to verify payment"});
+    }
+};
+
+export {placeOrder, verifyOrder};
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,18 @@
 import jwt from 'jsonwebtoken';
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
     const {token} = req.headers;
     if(!token){
         return res.json({success:false, message:"Not Authorized Login Again"})
     }
     try{
-        const token_decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.body.userId = token_decoded.id;
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+        req.body.userId = token_decode.id;
         next();
     }
     catch(error){
         console.log(error);
-        res.json({success:false, message:"Error"});
+        res.json({success:false, message:"authError"});
     }
 }
 
